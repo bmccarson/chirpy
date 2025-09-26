549247dc1765d009e661e5dc5e9cc450ead067a5
bmccarson <brian@mccarson.us>
2025-09-25 22:09:02 -0400

    fix server init

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ func main() {
 	serverMux := http.NewServeMux()
 	serverMux.Handle("/", http.FileServer(http.Dir("")))
 
-	server := http.Server{
+	server := &http.Server{
 		Handler: serverMux,
 		Addr:    ":" + port,
 	}
 
-	fmt.Printf("Serving on port %s", port)
+	fmt.Printf("Serving on port %s\n", port)
 	log.Fatal(server.ListenAndServe())
 }
