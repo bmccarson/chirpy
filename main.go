1c0dc978f0ee33d0974781b406b163c52223f8cd
bmccarson <brian@mccarson.us>
2025-09-26 16:50:08 -0400

    add middleware for counting api hits

--- main.go
+++ main.go
@@ -5,15 +5,44 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerHits(w http.ResponseWriter, _ *http.Request) {
+	hits := cfg.fileserverHits.Load()
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, fmt.Sprintf("Hits: %d", hits))
+}
+
+func (cfg *apiConfig) handlerResetHits(w http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "Hits Reset")
+}
 
 func main() {
 	const port = "8080"
 
+	apiCfg := &apiConfig{}
+
 	serverMux := http.NewServeMux()
-	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	serverMux.Handle("/app/assets", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets"))))
+	serverMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	serverMux.HandleFunc("/healthz", handlerReady)
+	serverMux.HandleFunc("/metrics", apiCfg.handlerHits)
+	serverMux.HandleFunc("/reset", apiCfg.handlerResetHits)
 
 	server := &http.Server{
 		Handler: serverMux,
