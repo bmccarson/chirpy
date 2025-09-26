a6035501bb60cc368edfd848c75fe40b92443c4a
bmccarson <brian@mccarson.us>
2025-09-25 22:53:19 -0400

    add health endpoint

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -10,7 +11,9 @@ func main() {
 	const port = "8080"
 
 	serverMux := http.NewServeMux()
-	serverMux.Handle("/", http.FileServer(http.Dir("")))
+	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serverMux.Handle("/app/assets", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets"))))
+	serverMux.HandleFunc("/healthz", handlerReady)
 
 	server := &http.Server{
 		Handler: serverMux,
@@ -19,4 +22,10 @@ func main() {
 
 	fmt.Printf("Serving on port %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReady(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
 }
