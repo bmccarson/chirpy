eefea0fcf76a7760a9a3c97318cbb07bed93aa77
bmccarson <brian@mccarson.us>
2025-09-25 21:58:25 -0400

    add basic file server

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	serverMux := http.NewServeMux()
+	serverMux.Handle("/", http.FileServer(http.Dir("")))
+
 	server := http.Server{
 		Handler: serverMux,
 		Addr:    ":8080",
